139655a6aeda8b3b77beab901cfb9a2aa59d4a23
michele.puricelli <michele.puricelli@bcode.cloud>
2024-05-07 20:45:54 +0200

    fix: added maxWidth

--- src/components/MainCard.tsx
+++ src/components/MainCard.tsx
@@ -7,10 +7,17 @@ type MainCardProps = {
 
 const MainCard = ({ children }: MainCardProps) => {
   return (
-    <Box height={'100dvh'} display="grid" alignItems="start" justifyItems="center">
+    <Box
+      maxWidth={'100dvw'}
+      height={'100dvh'}
+      display="grid"
+      alignItems="start"
+      justifyItems="center"
+      maxHeight={'100dvh'}
+    >
       <Paper
         elevation={3}
-        sx={{ p: { xs: '2rem', sm: '3rem', md: '3rem' }, width: { sm:'90%', md: '70%' } }}
+        sx={{ p: { xs: '2rem', sm: '3rem', md: '3rem' }, width: { sm: '90%', md: '70%' } }}
       >
         {children}
       </Paper>
